6a53d34f3f318e644f94381e99f662252ef9ecb9
blakeparkinson <blake@edmodo.com>
2014-12-27 22:28:40 -0600

    sample api call

--- app.js
+++ app.js
@@ -56,5 +56,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
+
 
 module.exports = app;
