4ab0c8bba94e9d46a56e8b4f57cfc3b1c06cde6d
blakeparkinson <blake@edmodo.com>
2015-01-03 01:11:14 -0800

    adding some teams

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var nba_teams = {
+    'NOP': 'New Orleans Pelicans',
+    'GSW': 'Golden State Warriors',
+    'SAS': 'San Antonio Spurts'
+}
+
+console.log(nba_teams);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { teams: nba_teams });
 });
 
 module.exports = router;
