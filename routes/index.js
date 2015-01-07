5cce69ce7b1dbe1520f11e88daedd3e5f8ea82ac
blakeparkinson <blake@edmodo.com>
2015-01-06 23:12:17 -0800

    removing

--- routes/index.js
+++ routes/index.js
@@ -13,29 +13,3 @@ router.get('/', function(req, res) {
 
 module.exports = router;
 
-
-// DOM Ready =============================================================
-$(document).ready(function() {
-
-    // Populate the user table on initial page load    
-    $('body').on('click', '[data-action="pick-team"]', fetchTeam);
-
-});
-
-// Functions ============================================================= //
-
-
-function fetchTeam(event) {
-    var team = $('#teams option:selected').val();
-    console.log(nba_key);
-
-    request('https://api.sportsdatallc.org/nba-t3/league/hierarchy?api_key=' + nba_key, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-  else{
-    console.log(error)
-  }
-})
-
-}
